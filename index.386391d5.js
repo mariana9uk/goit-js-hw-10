ffe6098d88bdd19c51a8e19cee0cc59ab5815636
mariana9uk <124436149+mariana9uk@users.noreply.github.com>
2023-07-11 09:54:38 +0000

    Deploying to gh-pages from @ mariana9uk/goit-js-hw-10@5926c62845b3920e9cd75fab0ed2a368c42238c4 🚀

---
+++ index.386391d5.js
@@ -0,0 +1,2 @@
+document.querySelector(".breed-select"),documennt.querySelector(".cat-info");
+//# sourceMappingURL=index.386391d5.js.map
---
+++ index.3af14e23.js
@@ -0,0 +1,2 @@
+document.querySelector(".breed-select"),documennt.querySelector(".cat-info");
+//# sourceMappingURL=index.3af14e23.js.map
