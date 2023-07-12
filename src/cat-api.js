57e671dffe16214a38d9590fcd874c8958a01b1e
mariana9uk <maryanagolyuk@gmail.com>
2023-07-12 23:51:43 +0300

    draft

--- src/cat-api.js
+++ src/cat-api.js
@@ -7,4 +7,14 @@ const fetchBreeds = () =>
     }
     return response.json();
   })
-  export {fetchBreeds};
+  
+function fetchCatByBreed(breedId){
+    const breedId = data.map(breed=>breed.id);
+  fetch("https://api.thecatapi.com/v1/images/search?breed_ids={breedId}")
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  })}
+  export {fetchBreeds, fetchCatByBreed}
