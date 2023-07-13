9f138a83e387e16c89529954dd8af3b32d43f497
mariana9uk <maryanagolyuk@gmail.com>
2023-07-14 01:43:42 +0300

    notiflix

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,12 +1,12 @@
 
-const fetchBreeds = () =>
-    fetch("https://api.thecatapi.com/v1/breeds")
+ function fetchBreeds(){
+  return fetch("https://api.thecatapi.com/v1/breeds")
   .then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
     return response.json();
-  })
+  })}
   // const breedId = data.map(breed=>breed.id);
 const fetchCatByBreed = (selectedBreedId) =>
 fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${selectedBreedId}`)
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import SlimSelect from 'slim-select';
 import axios from "axios";
+import Notiflix from 'notiflix'
 axios.defaults.headers.common["x-api-key"] = "live_miOQ9IJmBTf4JhWdnWNTcaxj7nGrlgj5kYjUnmo8YzynBCK5HwOcOpOl4BaI5vhv";
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 const catInfoWrapEl = document.querySelector('.cat-info');
 const selectorEl = document.querySelector('.breed-select');
+const errorEl = document.querySelector('.error')
 
   fetchBreeds()
   .then(data => {
@@ -26,44 +28,30 @@ breedSelect.setData(selectData);
   .catch(error => {
  console.log("Error!", error)
   });
-
-
   const handleCatInfoSubmit = (event) =>{
     event.preventDefault()
     const selectedBreedId = event.target.value;
     console.log(selectedBreedId)
    fetchCatByBreed(selectedBreedId)
          .then(data => {
-    
-    console.log(data)
-
-    catInfoWrapEl.innerHTML = `
+          if (data && data.length > 0){
+       catInfoWrapEl.innerHTML = `
     <div>
       <img src="${data[0].url}" alt="Cat Image">
       <p>Назва породи: ${data[0].breeds[0].name}</p>
       <p>Опис: ${data[0].breeds[0].description}</p>
       <p>Темперамент: ${data[0].breeds[0].temperament}</p>
     </div>
-  `;
-      // renderBreed(data)
+  `;}
+     else{
+      console.log("No data")
+     } // renderBreed(data)
   })
   .catch(error => {
-    console.log("Woops!", error);
+    console.log("Woops!", error)
+    Notiflix.Notify.failure('❌Помилка');
   });
 };
-// const renderBreed = (data) => {
-
-//   const cat = data[0];
-//   const markup = `
-//     <div>
-//       <img src="${cat.url}" alt="Cat Image">
-//       <p>Назва породи: ${cat.breeds[0].name}</p>
-//       <p>Опис: ${cat.breeds[0].description}</p>
-//       <p>Темперамент: ${cat.breeds[0].temperament}</p>
-//     </div>
-//   `;
-//   catInfoWrapEl.innerHTML = markup; 
-// };
 
   selectorEl.addEventListener('change', handleCatInfoSubmit)
 
