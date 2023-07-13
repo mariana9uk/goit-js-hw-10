f3a12dafa3247f6a046ff52bd28ffbc6b78517ac
mariana9uk <maryanagolyuk@gmail.com>
2023-07-13 23:39:32 +0300

    changes

--- src/cat-api.js
+++ src/cat-api.js
@@ -7,14 +7,15 @@ const fetchBreeds = () =>
     }
     return response.json();
   })
-  
-function fetchCatByBreed(breedId){
-    const breedId = data.map(breed=>breed.id);
-  fetch("https://api.thecatapi.com/v1/images/search?breed_ids={breedId}")
+  // const breedId = data.map(breed=>breed.id);
+const fetchCatByBreed = (selectedBreedId) =>
+fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${selectedBreedId}`)
   .then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
     return response.json();
-  })}
+  })
+
+
   export {fetchBreeds, fetchCatByBreed}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import SlimSelect from 'slim-select';
 import axios from "axios";
 axios.defaults.headers.common["x-api-key"] = "live_miOQ9IJmBTf4JhWdnWNTcaxj7nGrlgj5kYjUnmo8YzynBCK5HwOcOpOl4BaI5vhv";
-import { fetchBreeds } from './cat-api';
-
-const catInfoEl = document.querySelector('.cat-info');
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
+const catInfoWrapEl = document.querySelector('.cat-info');
 const selectorEl = document.querySelector('.breed-select');
 
   fetchBreeds()
@@ -27,6 +26,48 @@ breedSelect.setData(selectData);
   .catch(error => {
  console.log("Error!", error)
   });
+
+
+  const handleCatInfoSubmit = (event) =>{
+    event.preventDefault()
+    const selectedBreedId = event.target.value;
+    console.log(selectedBreedId)
+   fetchCatByBreed(selectedBreedId)
+         .then(data => {
+    
+    console.log(data)
+
+    catInfoWrapEl.innerHTML = `
+    <div>
+      <img src="${data[0].url}" alt="Cat Image">
+      <p>Назва породи: ${data[0].breeds[0].name}</p>
+      <p>Опис: ${data[0].breeds[0].description}</p>
+      <p>Темперамент: ${data[0].breeds[0].temperament}</p>
+    </div>
+  `;
+      // renderBreed(data)
+  })
+  .catch(error => {
+    console.log("Woops!", error);
+  });
+};
+// const renderBreed = (data) => {
+
+//   const cat = data[0];
+//   const markup = `
+//     <div>
+//       <img src="${cat.url}" alt="Cat Image">
+//       <p>Назва породи: ${cat.breeds[0].name}</p>
+//       <p>Опис: ${cat.breeds[0].description}</p>
+//       <p>Темперамент: ${cat.breeds[0].temperament}</p>
+//     </div>
+//   `;
+//   catInfoWrapEl.innerHTML = markup; 
+// };
+
+  selectorEl.addEventListener('change', handleCatInfoSubmit)
+
+
   // fetch("https://api.thecatapi.com/v1/breeds")
   // .then(response => {
   //   if (!response.ok) {
