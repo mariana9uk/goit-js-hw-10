84b7009743afdc1ddf1e660b631e31ac44c58963
mariana9uk <maryanagolyuk@gmail.com>
2023-07-14 12:44:51 +0300

    changes258

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ axios.defaults.headers.common["x-api-key"] = "live_miOQ9IJmBTf4JhWdnWNTcaxj7nGrl
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 const catInfoWrapEl = document.querySelector('.cat-info');
 const selectorEl = document.querySelector('.breed-select');
-const errorEl = document.querySelector('.error')
-
+const alertPopup = document.querySelector('.error')
+let isAlertVisible = false
   fetchBreeds()
-  .then(data => {
-console.log(data);
+  .then(breedsData => {
+console.log(breedsData);
 const breedSelect = new SlimSelect({
   select: selectorEl,
   settings: {
@@ -19,7 +19,7 @@ const breedSelect = new SlimSelect({
       searchHighlight: true,
     },   
 })
-const selectData = data.map(breed => ({
+const selectData = breedsData.map(breed => ({
   text: breed.name,
   value: breed.id
 }))
@@ -28,32 +28,44 @@ breedSelect.setData(selectData);
   .catch(error => {
  console.log("Error!", error)
   });
+
+
   const handleCatInfoSubmit = (event) =>{
     event.preventDefault()
     const selectedBreedId = event.target.value;
     console.log(selectedBreedId)
+
    fetchCatByBreed(selectedBreedId)
          .then(data => {
-          if (data && data.length > 0){
-       catInfoWrapEl.innerHTML = `
-    <div>
-      <img src="${data[0].url}" alt="Cat Image">
-      <p>Назва породи: ${data[0].breeds[0].name}</p>
-      <p>Опис: ${data[0].breeds[0].description}</p>
-      <p>Темперамент: ${data[0].breeds[0].temperament}</p>
-    </div>
-  `;}
-     else{
-      console.log("No data")
-     } // renderBreed(data)
+          console.log(data)
+          renderCatData(data)
   })
   .catch(error => {
-    console.log("Woops!", error)
-    Notiflix.Notify.failure('❌Помилка');
+       console.log("Woops!", error)
+           Notiflix.Notify.failure('❌Помилка');
   });
 };
-
+function renderCatData(data){
+  
+  const markup = `
+      <img src="${data[0].url}" alt="Cat Image">
+    <p>Назва породи: ${data[0].breeds[0].name}</p>
+    <p>Опис: ${data[0].breeds[0].description}</p>
+    <p>Темперамент: ${data[0].breeds[0].temperament}</p>`
+    catInfoWrapEl.insertAdjacentHTML("beforeend", markup);
+}
   selectorEl.addEventListener('change', handleCatInfoSubmit)
+
+  function toggleAlertPopup() {
+    if (isAlertVisible) {
+      return;
+    }
+    isAlertVisible = true;
+    alertPopup.classList.add("is-visible");
+    setTimeout(() => {
+      alertPopup.classList.remove("is-visible");
+      isAlertVisible = false;
+    }, 3000)}
 
 
   // fetch("https://api.thecatapi.com/v1/breeds")
